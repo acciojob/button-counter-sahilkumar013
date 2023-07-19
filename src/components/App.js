6011cd2e7c8894d2c8136b7b717fa20d95de3a97
sahilkumar013 <sahilkashyap7056@gmail.com>
2023-07-19 13:03:30 +0530

    submitting code-Wed Jul 19 2023 13:02:16 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,11 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [count,setCount] = useState(0);
   return (
     <div>
-        {/* Do not remove the main div */}
+        <p>Button clicked {count}</p>
+        <button onClick={() => setCount(count+1)}>Click me</button>
     </div>
   )
 }
